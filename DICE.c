a83e858d0de76532c66db1174541b60412be8eb3
Ryo_Otake <s1250211@gmail.com>
2019-07-11 16:51:13 +0900

    New Function added

--- DICE.c
+++ DICE.c
@@ -1,22 +1,39 @@
 #include<stdio.h>
 #include<time.h>
 int main(){
-  int Dice1,Dice2,Total;
-  char name[15];
-  printf("What is your name?\n");
+  int Dice1_p1,Dice2_p1,Dice1_p2,Dice2_p2,Total,Total1;
+  char name[15],name1[15];
+  printf("What is plater1's name?\n");
   scanf("%s",name);
-  printf("Hellow,%s !\n",name);
+  printf("Hellow,%s !\n\n",name);
+  printf("What is plater2's name?\n");
+  scanf("%s",name1);
+  printf("Hellow,%s !\n\n",name1);
+  
   printf("Roling the dice...\n");
   srand(time(NULL));
-  Dice1=rand()%6+1;
-  Dice2=rand()%6+1;
-  Total = Dice1+Dice2;
-  printf("Dice1 : %d\n",Dice1);
-   printf("Dice2 : %d\n",Dice2);
-   printf("Total value : %d\n",Total);
-   if(Total>7)printf("%s Won !\n",name);
-   else printf("%s Lost.\n",name);
+  Dice1_p1=rand()%6+1;
+  Dice2_p1=rand()%6+1;
+  Dice1_p2=rand()%6+1;
+  Dice2_p2=rand()%6+1;
+  Total = Dice1_p1+Dice2_p1;
+  Total1= Dice1_p2+Dice2_p2,
+  
+  printf("Player1 Dice1 : %d\n",Dice1_p1);
+   printf("Player1 Dice2 : %d\n",Dice2_p1);
+   printf("Player1 Total value : %d\n\n",Total);
 
+   printf("Player2 Dice1 : %d\n",Dice1_p2);
+   printf("Player2 Dice2 : %d\n",Dice2_p2);
+   printf("Player2 Total value : %d\n\n",Total1);
+   
+   if(Total>Total1){printf("%s Won !\n",name);
+   printf("%s Lost.\n",name1);
+   }
+   else {printf("%s Won !\n",name1);
+   printf("%s Lost.\n",name);
+   }
+   
   return 0;
  
 }
